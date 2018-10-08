90b62b5426992d5863c20ac65bfa9d90a773f55c
adamsvystun <adam.svystun@gmail.com>
2018-10-08 23:02:33 +0200

    Improve parse_log.py

--- parse_log.py
+++ parse_log.py
@@ -16,6 +16,9 @@ $ python parse_log.py -p blah_blah_blah/log_train.txt
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--path', type=str, required=True, help="path to log.txt; output saved to the same dir")
+parser.add_argument('-se', '--start-epoch', type=int, required=False, help="Starting epoch to plot from. (default: 0)", default=0)
+parser.add_argument('-ee', '--end-epoch', type=int, required=False, help="Ending epoch (default: last)", default=None)
+parser.add_argument('-f', '--filename', type=str, required=False, help="Filename to save the plot to. (default: overall_reward.png)", default="overall_reward.png")
 args = parser.parse_args()
 
 if not osp.exists(args.path):
@@ -35,9 +38,16 @@ with open(args.path, 'r') as f:
             reward = float(reward_match.group(1))
             rewards.append(reward)
 
-plt.plot(rewards)
+epochs = list(xrange(len(rewards)))
+if args.start_epoch:
+    rewards = rewards[args.start_epoch:]
+    epochs = epochs[args.start_epoch:]
+if args.end_epoch:
+    rewards = rewards[:args.end_epoch]
+    epochs = epochs[:args.end_epoch]
+plt.plot(epochs, rewards)
 plt.xlabel('epoch')
 plt.ylabel('reward')
 plt.title("Overall rewards")
-plt.savefig(osp.join(osp.dirname(args.path), 'overall_reward.png'))
+plt.savefig(osp.join(osp.dirname(args.path), args.filename))
 plt.close()
